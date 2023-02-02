1b4129f44e8145586f74dc5bb45e33503c3b5de5
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-01 20:53:10 -0500

    initial commit

---
+++ poolcleaner.py
@@ -0,0 +1,9 @@
+## Game State
+game = {"tool": 0, "money": 0}
+
+tools = {
+{"name" : "Small Net", "profit": 10, "cost": 0},
+{"name" : "Big Net", "profit": 100, "cost": 100}
+}
+
+## Game Option Function
